ca1581315e369aa0b9b7dbd3296e13202cfd3f4c
hoptond <hoptond848@protonmail.com>
2018-12-10 20:33:46 +0000

    add gamescreen class

---
+++ src/Classes/Gamescreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Gamescreen extends Component {
+	
+	render() {
+		return (
+		<div className="gamescreen">
+			</div>
+			);
+	}
+}
+
+export default Gamescreen
