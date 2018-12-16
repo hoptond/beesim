703459ec31254feeed6b2366cf5f216163c25189
hoptond <hoptond848@protonmail.com>
2018-12-16 19:01:21 +0000

    implement ability to toggle panel

--- src/Classes/UiPanel.js
+++ src/Classes/UiPanel.js
@@ -4,16 +4,34 @@ class UiPanel extends Component {
 	
 	constructor(props) {
 		super(props)
+		this.state = {
+			active: true
+		}
 	}
 	
+	//TODO: move the panel offscreen by dragging it rather than doing onClick
 	render() {
 		return (
-		<div className="uipanel">
+		<div className={this.getClassName()} onClick={(e) => this.togglePanel(e)}>
 			{this.props.children}
 			</div>
 			);
 	}
 	
+	getClassName() {
+		let output = "uipanel"
+		return output += this.state.active ? "" : " offscreen"
+	}
+	
+	togglePanel = (e) => {
+		if(e.target.className !== "uibutton") {
+			this.setState({ 
+				active: !this.state.active
+			})
+		}
+
+	}
+
 }
 
 export default UiPanel
